ec6fb1a80dcb402ac502e5ef803a5c2958e444e2
Toly <suvorov.aa@students.dvfu.ru>
2022-03-07 12:33:46 +1000

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+if (a<b) 
+{
+	cin >> a >> b;
+	cout << "Введите 2 числа";
+}
+else if (a>b) 
+{
+	cin >> a >> b;
+	cout << "a";
+}
+else 
+{
+	cin >> a >> b;
+	cout << "Числа ровны";
+}
+return 0;
+}
